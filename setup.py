9a1b9faa2b17e24f36f1e037c6b7fe153b780075
Stanisław Szufa <s.szufa@gmail.com>
2020-02-12 15:08:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ import setuptools
 from setuptools import setup
 
 setup(name='map-of-elections',
-      version='1.5.2',
+      version='1.0.0',
       description='Map of Elections',
-      url='https://github.com/szufix/mapel',
+      url='https://github.com/szufix/mapel',      
+      download_url='https://github.com/Koozco/pmp/archive/v1.0.0.tar.gz',
       author='Stanislaw Szufa',
       author_email='s.szufa@gmail.com',
       packages=setuptools.find_packages(),
