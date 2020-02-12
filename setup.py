4b6fbe2c6fc939e0940a2a2adcd971f097f9b221
Stanisław Szufa <s.szufa@gmail.com>
2020-02-12 14:50:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 
 from distutils.core import setup
 setup(name='src',
+      author='Stanislaw Szufa',
+      author_email='s.szufa@gmail.com',
       version='1.0',
       py_modules=['src'],
       )
