b554fba92c64c3ce1f2d4f029ce5d4228053548e
Stanisław Szufa <s.szufa@gmail.com>
2020-02-12 15:02:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 
-from distutils.core import setup
-setup(name='src',
+import setuptools
+from setuptools import setup
+
+setup(name='map-of-elections',
+      version='1.5.2',
+      description='Map of Elections',
+      url='https://github.com/szufix/mapel',
       author='Stanislaw Szufa',
       author_email='s.szufa@gmail.com',
-      version='1.0',
-      py_modules=['src'],
-      )
+      license='MIT',
+      packages=setuptools.find_packages(),
+      install_requires=['numpy'],
+      zip_safe=False)
