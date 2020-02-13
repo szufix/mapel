d257d9e2c55ca55ac04830ea7d14faa4f5bf1c48
Stanisław Szufa <s.szufa@gmail.com>
2020-02-13 16:10:35 +0100

    Update __init__.py

--- mapel/__init__.py
+++ mapel/__init__.py
@@ -1,2 +1,13 @@
+import voting.modern as mo
+
 def hello():
     print("hello")
+
+def test():
+
+    #name = str(sys.argv[1])
+    name = "example_100_20"
+
+    #mo.convert_xd_to_2d(name, num_iterations=1000)
+
+    mo.print_2d(name, shades=False)
