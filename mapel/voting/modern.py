5e7652858478e232910f550eff2ce27461c44b8e
Stanisław Szufa <s.szufa@gmail.com>
2020-02-14 16:50:59 +0100

    Update modern.py

--- mapel/voting/modern.py
+++ mapel/voting/modern.py
@@ -1,44 +1,167 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from . import objects as obj
+#!/usr/bin/env python
+
+import math
 import os
 
 
-def print_2d(name, num_winners=0, shades=False):
-    model = obj.Model_2d(name, num_winners=num_winners, shades=shades)
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+class Model_xd:
 
-    if shades:
-        file_name = "results/times/" + str(name) + ".txt"
+    def __init__(self, name):
+        self.name = name
+        self.num_points, self.num_distances, self.distances = self.import_distances(name)
+        self.num_families, self.family_name, self.family_special, self.family_size, \
+            self.labels, self.colors, self.alphas = self.import_controllers(name)
+
+    @staticmethod
+    def import_distances(name):
+
+        file_name = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+        file_name = os.path.join(file_name, "results", "distances", str(name) + ".txt")
         file_ = open(file_name, 'r')
+        num_points = int(file_.readline())
+        num_families = int(file_.readline())
+        num_distances = int(file_.readline())
+
+        hist_data = [[0 for _ in range(num_points)] for _ in range(num_points)]
+
+        for a in range(num_points):
+            for b in range(a + 1, num_points):
+                line = file_.readline()
+                line = line.split(' ')
+                hist_data[a][b] = float(line[2])
+
+        return num_points, num_distances, hist_data
+
+    @staticmethod
+    def import_controllers(name):
+
+        file_name = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+        file_name = os.path.join(file_name, "controllers", "models", str(name) + ".txt")
+        file_ = open(file_name, 'r')
+        num_voters = int(file_.readline())
+        num_candidates = int(file_.readline())
+        num_families = int(file_.readline())
+        family_name = [0 for _ in range(num_families)]
+        family_special = [0 for _ in range(num_families)]
+        family_size = [0 for _ in range(num_families)]
+        labels = [0 for _ in range(num_families)]
+        colors = [0 for _ in range(num_families)]
+        alphas = [0 for _ in range(num_families)]
+
+        for i in range(num_families):
+            line = file_.readline().rstrip("\n").split(',')
+            family_name[i] = str(line[1])
+            family_special[i] = float(line[2])
+            family_size[i] = int(line[0])
+            labels[i] = str(line[5])
+            colors[i] = str(line[3].replace(" ", ""))
+            alphas[i] = float(line[4])
+
+        file_.close()
+        return num_families, family_name, family_special, family_size, labels, colors, alphas
+
+
+class Model_2d:
+
+    def __init__(self, name, num_winners=0, shades=False):
+
+        self.name = name
+        self.num_points, self.points, = self.import_points(name)
+        self.num_families, self.family_name, self.family_special, self.family_size, \
+            self.labels, self.colors, self.alphas = self.import_controllers(name)
+        self.points_by_families = self.compute_points_by_families()
+
+        if num_winners > 0:
+            self.winners = self.import_winners(name, num_winners)
+        else:
+            self.winners = []
+
+        if shades:
+            print("shades")
+        else:
+            self.shades = [1. for _ in range(self.num_points)]
+
+    @staticmethod
+    def import_points(name):
+
+        file_name = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+        file_name = os.path.join(file_name, "results", "points", str(name) + ".txt")
+        file_ = open(file_name, 'r')
+        num_winners = int(file_.readline())
+        points = [[0, 0] for _ in range(num_winners)]
+
+        for i in range(num_winners):
+            line = file_.readline().replace("\n", '').split(',')
+            points[i] = [float(line[0]), float(line[1])]
+
+        file_.close()
+        return len(points), points
+
+    @staticmethod
+    def import_controllers(name):
+
+        file_name = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+        file_name = os.path.join(file_name, "controllers", "models", str(name) + ".txt")
+        file_ = open(file_name, 'r')
+        num_voters = int(file_.readline())
+        num_candidates = int(file_.readline())
+        num_families = int(file_.readline())
+        family_name = [0 for _ in range(num_families)]
+        family_special = [0 for _ in range(num_families)]
+        family_size = [0 for _ in range(num_families)]
+        labels = [0 for _ in range(num_families)]
+        colors = [0 for _ in range(num_families)]
+        alphas = [0 for _ in range(num_families)]
+
+        for i in range(num_families):
+            line = file_.readline().rstrip("\n").split(',')
+            family_name[i] = str(line[1])
+            family_special[i] = float(line[2])
+            family_size[i] = int(line[0])
+            labels[i] = str(line[5])
+            colors[i] = str(line[3].replace(" ", ""))
+            alphas[i] = float(line[4])
+
+        file_.close()
+        return num_families, family_name, family_special, family_size, labels, colors, alphas
+
+    @staticmethod
+    def import_winners(name, first_winners):
+
+        file_name = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+        file_name = os.path.join(file_name, "results", "winners", str(name) + "_cc.txt")
+        file_ = open(file_name, 'r')
+        number_of_elections = int(file_.readline())
+        real_num_winners = int(file_.readline())
+        real_num_winners = first_winners
+        real_winners = []
+        time = float(file_.readline())
+
+        for w in range(real_num_winners):
+            real_winners.append(int(file_.readline()))
+
+        return real_winners
+
+    #@staticmethod
+    def compute_points_by_families(self):
+
+        points_by_families = [[[] for _ in range(2)] for _ in range(self.num_points)]
         ctr = 0
-        for k in range(model.num_families):
-            for _ in range(model.family_size[k]):
-                if ctr < 200:
-                    shade = float(file_.readline())
-                else:
-                    shade = 1.
-                ax.scatter(model.points[ctr][0], model.points[ctr][1],
-                           color=model.colors[k], alpha=model.alphas[k]*shade, s=9)
+
+        for i in range(self.num_families):
+            for j in range(self.family_size[i]):
+                points_by_families[i][0].append(self.points[ctr][0])
+                points_by_families[i][1].append(self.points[ctr][1])
                 ctr += 1
 
-    else:
-        #for w in model.winners:
-        #for w in [513, 527]:
-        #    ax.scatter(model.points[w][0], model.points[w][1], color="black", s=30)
-        for k in range(model.num_families):
-            ax.scatter(model.points_by_families[k][0], model.points_by_families[k][1],
-                       color=model.colors[k], label=model.labels[k],
-                       alpha=model.alphas[k], s=9)
+        return points_by_families
 
-    box = ax.get_position()
-    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    #@staticmethod
+    def get_distance(self, i, j):
 
-    file_name = str(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../'))
-    file_name += "images/" + str(name) + ".png"
-    plt.savefig(file_name)
-    plt.show()
+        distance = 0.
 
+        for d in range(2):
+            distance += (self.points[i][d] - self.points[j][d]) ** 2
 
+        return math.sqrt(distance)
