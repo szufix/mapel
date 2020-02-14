8cadde93f7969f0fbd1f11491f2ebb56a31a6485
Stanisław Szufa <s.szufa@gmail.com>
2020-02-14 15:44:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,7 @@ from setuptools import setup
 def readme():
     with open('README.md') as f:
         return f.read()
-    
-mapel_dir_1 = os.path.join('results', 'points')
-mapel_dir_2 = os.path.join('controllers', 'models')
-      
+          
 setup(name='map-of-elections',
       version='1.0.5',
       description='Map of Elections',
@@ -16,7 +13,7 @@ setup(name='map-of-elections',
       download_url='https://github.com/szufix/mapel/archive/v1.0.5.tar.gz',
       author='Stanislaw Szufa',
       author_email='s.szufa@gmail.com',
-      package_data={'mapel_1': [mapel_dir_1 + '/*.txt'], 'mapel_2': [mapel_dir_2 + '/*.txt']},
+      package_data={'mapel': ['/*.txt']},
       packages=setuptools.find_packages(),
       install_requires=['numpy'],
       zip_safe=False)
