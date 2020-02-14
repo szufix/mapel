6da6d8acf801de533610fb8686fc285403b818af
Stanisław Szufa <s.szufa@gmail.com>
2020-02-14 15:37:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
     with open('README.md') as f:
         return f.read()
     
-mapel_dir = os.path.join('results')
+mapel_dir = os.path.join('results', 'points')
       
 setup(name='map-of-elections',
       version='1.0.5',
