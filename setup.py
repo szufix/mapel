42d95f0c574c24380e71b70bd5adaa2120ccbeac
Krzysiek <krzychu.janiszewski@gmail.com>
2020-02-15 11:22:53 +0000

    pip

--- setup.py
+++ setup.py
@@ -1,27 +1,35 @@
+from setuptools import setup, find_packages
 import os
-import setuptools
-from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-    
-mapel_dir_1 = os.path.join('controllers','models')
-mapel_dir_2 = os.path.join('results','points')
+rootDir = os.path.abspath(os.path.dirname(__file__))
+reqPath = os.path.join(rootDir, 'requirements.txt')
+readmePath = os.path.join(rootDir, 'README.md')
+mapel_dir_1 = os.path.join(rootDir, 'controllers', 'models')
+mapel_dir_2 = os.path.join(rootDir, 'results', 'points')
 
-setup(name='map-of-elections',
-      version='1.0.7',
-      description='Map of Elections',
-      url='https://github.com/szufix/mapel',      
-      download_url='https://github.com/szufix/mapel/archive/v1.0.7.tar.gz',
-      author='Stanislaw Szufa',
-      author_email='s.szufa@gmail.com',
-      package_data={'mapel': [mapel_dir_1 + '/*.txt', mapel_dir_2 + '/*.txt']},
-      packages=setuptools.find_packages(),
-      install_requires=['matplotlib', 'numpy'],
-      classifiers=[
+with open(reqPath) as f:
+    required = f.read().splitlines()
+
+with open(readmePath, "r") as f:
+    long_description = f.read()
+
+setup(
+    name='mapel',
+    version='1.0.7',
+    license='MIT',
+    author='Stanislaw Szufa',
+    author_email='s.szufa@gmail.com',
+    description='Map of Elections',
+    long_description=long_description,
+    url='https://github.com/szufix/mapel',
+    download_url='https://github.com/szufix/mapel/archive/v1.0.7.tar.gz',
+    package_data={'mapel': [mapel_dir_1 + '/*.txt', mapel_dir_2 + '/*.txt']},
+    packages=find_packages(),
+    install_requires=required,
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        ],
-        python_requires='>=3.6',
-    )
+    ],
+    python_requires='>=3.6',
+)
