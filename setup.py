2ff6e2d648d218240d44387f1a7a83cb2410127d
Stanisław Szufa <s.szufa@gmail.com>
2020-04-21 15:36:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(readmePath, "r") as f:
  
 setup(
     name='mapel',
-    version='1.0.4',
+    version='1.0.5',
     license='MIT',
     author='Stanislaw Szufa',
     author_email='s.szufa@gmail.com',
