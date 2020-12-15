8bcc23cc910c87e738ff7fcef9ca09a1035e5155
Stanisław Szufa <s.szufa@gmail.com>
2020-12-15 13:46:00 +0100

    minor error

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(readmePath, "r") as f:
  
 setup(
     name='mapel',
-    version='1.1.4',
+    version='1.1.5',
     license='MIT',
     author='Stanislaw Szufa',
     author_email='s.szufa@gmail.com',
