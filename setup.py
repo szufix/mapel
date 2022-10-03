d493a87fcfa778041716118b73dcfb62d3dfbc8b
Andrzej Kaczmarczyk <andrzej.kaczmarczyk@agh.edu.pl>
2022-10-03 16:00:15 +0200

    Improving setup.py and pyproject.toml; cleaning the MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,19 +1,10 @@
 from setuptools import setup, find_packages
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
-cpp_dist_module = Pybind11Extension('mapel.elections.metrics.cppdistances',
+cpp_dist_module = Pybind11Extension('elections.metrics.cppdistances',
 ['mapel/elections/metrics/cppdistances.cpp'])
 
-print("asdfasdfasdfasdfasdf")
-
-print(find_packages())
-
-print(find_packages(exclude = ['mapel.elections.not_in_the_package*',
-    'mapel.elections.not_in_the_package']))
-
 setup(
-    packages=find_packages(exclude = ['mapel.elections.not_in_the_package*',
-    'mapel.elections.not_in_the_package']),
     ext_modules = [cpp_dist_module],
     cmdclass={"build_ext": build_ext},
 )
