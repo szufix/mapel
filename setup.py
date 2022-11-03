5d92a5a8c7fa58b46596287a3750501fae4acab7
Andrzej Kaczmarczyk <andrzej.kaczmarczyk@agh.edu.pl>
2022-11-03 11:04:30 +0100

    Fixing the project structure to prepare for tests.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
-cpp_dist_module = Pybind11Extension('elections.metrics.cppdistances',
-['mapel/elections/metrics/cppdistances.cpp'], extra_compile_args=["-Ofast"])
+cpp_dist_module = Pybind11Extension('mapel.elections.metrics.cppdistances',
+['src/mapel/elections/metrics/cppdistances.cpp'], extra_compile_args=["-Ofast"])
 
 setup(
     ext_modules = [cpp_dist_module],
--- src/mapel/allocations/cultures/basic_cultures.py
+++ src/mapel/allocations/cultures/basic_cultures.py
@@ -1,15 +1,14 @@
 import mapel.main.logs as logs
 logger = logs.get_logger(__name__)
-from mapel.allocations.core.alloctask import AllocationTask
 
-def identity_alloc_task(agents_cnt, resources_cnt, total_utility = 100, instance_id = "ID"):
-  logger.debug("Creating the ID allocation task")  
+def identity_alloct_matrix(agents_cnt, resources_cnt, total_utility = 100):
+  logger.debug("Creating the ID allocation task matrix")  
   alloct_matrix  = [([100] + [0]*(resources_cnt-1)) for _ in range(agents_cnt)]
-  return AllocationTask.from_matrix(alloct_matrix, instance_id)
+  return alloct_matrix
 
-def uniformity_alloc_task(agents_cnt, resources_cnt, total_utility = 100, instance_id = "UN"):
+def uniformity_alloct_matrix(agents_cnt, resources_cnt, total_utility = 100):
   import random
-  logger.debug("Creating the UN allocation task")  
+  logger.debug("Creating the UN allocation task matrix")  
   util_per_res_floor = total_utility / resources_cnt
   remaining_util = total_utility - (util_per_res_floor * resources_cnt)
   alloct_matrix = []
@@ -19,13 +18,13 @@ def uniformity_alloc_task(agents_cnt, resources_cnt, total_utility = 100, instan
       agents_with_margin = random.sample(range(resources_cnt), remaining_util)
       for a in agents_with_margin:
         alloct_matrix[-1][a] += 1
-  return AllocationTask.from_matrix(alloct_matrix, instance_id)
+  return alloct_matrix
 
-def separability_alloc_task(agents_cnt, resources_cnt, total_utility = 100, instance_id = "SEP"):
-  logger.debug("Creating the SEP allocation task")  
+def separability_alloct_matrix(agents_cnt, resources_cnt, total_utility = 100):
+  logger.debug("Creating the SEP allocation task matrix")  
   alloct_matrix = []
   for a in range(agents_cnt):
     preceeding_zeros = a % resources_cnt
     trailing_zeros = resources_cnt - preceeding_zeros - 1
     alloct_matrix.append([0]*preceeding_zeros + [total_utility] + [0]*trailing_zeros)
-  return AllocationTask.from_matrix(alloct_matrix, instance_id)
+  return alloct_matrix
