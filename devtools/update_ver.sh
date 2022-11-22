7387d4689b751662fdea6f01be1578aff55094ea
Andrzej Kaczmarczyk <andrzej.kaczmarczyk@agh.edu.pl>
2022-11-22 13:27:40 +0100

    Added a simple versioning script and updated the version.

---
+++ devtools/update_ver.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Gimme the version number that I should update the metadata to!"
+    exit 1
+fi
+
+MAPELDIR=..
+
+find "$MAPELDIR" -name 'pyproject.toml' |\
+  xargs -I % sed -i\
+  's\^version[[:blank:]]*=.*$\version = "'$1'"\g' %
+
+echo "Version updated to "$1
+
+
